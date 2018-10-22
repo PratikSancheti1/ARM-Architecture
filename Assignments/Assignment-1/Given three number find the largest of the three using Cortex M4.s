032af95d2e6ee172aa50ff1af15777c961d6f8a5
pratik-sancheti <44333402+pratik-sancheti@users.noreply.github.com>
2018-10-22 11:13:19 +0530

    Create Given three number find the largest of the three using Cortex M4.s

---
+++ Assignments/Assignment-1/Given three number find the largest of the three using Cortex M4.s
@@ -0,0 +1,17 @@
+     AREA     appcode, CODE, READONLY
+     export __main	 
+	 ENTRY 
+__main  function
+	          MOV r0 , #20   ;first number
+	          MOV r1 , #36    ;second number
+              MOV r2 , #10 	  ;third number  			  
+              CMP r0 , r1
+              IT HI
+              MOVHI r1 , r0   ;if value in r0>r1, move value in r0 to r1 
+			  CMP r1 , r2
+			  IT HI 
+			  MOVHI r2 , r1    ;if value in r1>r2, move value in r1 to r2
+			  MOV r3 , r2      ; largest number is seen in r3    
+STOP		      B STOP  ; stop program
+        endfunc
+      end
